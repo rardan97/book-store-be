d4150ce89c2e56210029f0ad1a0bc562d8e58cfa
rardan97 <risalardan@gmail.com>
2025-07-22 17:14:33 +0700

    update project

--- src/main/java/com/blackcode/book_store_be/config/WebConfig.java
+++ src/main/java/com/blackcode/book_store_be/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .allowCredentials(true);
+                .allowedHeaders("Authorization", "Content-Type");
+//                .allowCredentials(true);
     }
 }
